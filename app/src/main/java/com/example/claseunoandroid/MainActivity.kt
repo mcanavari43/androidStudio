ed429b1aa9a9955df1b3a4a4544e1a13b64e2a2a
Manu43 <manuel.canavari43@gmail.com>
2024-08-18 13:50:48 +0200

    Modificando Text cuando inicia App

--- app/src/main/java/com/example/claseunoandroid/MainActivity.kt
+++ app/src/main/java/com/example/claseunoandroid/MainActivity.kt
@@ -13,13 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-    }
-
+    val helloTextView = findViewById<TextView>(R.id.texto_identificador);
+    helloTextView.setText("EAEAEAEA");
+}
 
 }
